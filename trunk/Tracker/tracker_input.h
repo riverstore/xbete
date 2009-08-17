afab08ccb13dec2c5b2e1c9f1778a6884307540e
YatsukSergey <YatsukSergey@51af2968-8813-32a0-5910-3e3735077a46>
2009-08-17 20:02:44 +0000

    Head revision

---
+++ trunk/Tracker/tracker_input.h
@@ -0,0 +1,39 @@
+#if !defined(AFX_TRACKER_INPUT_H__E4F9E6ED_97B7_4526_B310_82F149E42EA8__INCLUDED_)
+#define AFX_TRACKER_INPUT_H__E4F9E6ED_97B7_4526_B310_82F149E42EA8__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class Ctracker_input
+{
+public:
+	void set(const string& name, const string& value);
+	bool valid() const;
+	bool banned() const;
+	Ctracker_input();
+
+	enum t_event
+	{
+		e_none,
+		e_completed,
+		e_started,
+		e_stopped,
+	};
+
+	t_event m_event;
+	string m_info_hash;
+	int m_ipa;
+	string m_key;
+	string m_peer_id;
+	long long m_downloaded;
+	long long m_left;
+	int m_port;
+	long long m_uploaded;
+	int m_num_want;
+	bool m_compact;
+	bool m_no_peer_id;
+	string m_agent;
+};
+
+#endif // !defined(AFX_TRACKER_INPUT_H__E4F9E6ED_97B7_4526_B310_82F149E42EA8__INCLUDED_)
